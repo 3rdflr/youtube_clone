230e66506f8ab63cb56456e22fc9aab56c34b698
3rdflr <3rdflrhtl@gmail.com>
2024-11-07 18:40:34 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,9 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log(`Somebody is tring to go home`);
+const handleHome = (req, res) => {
+  return res.send("<h1></h1>");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("<h1></h1>");
+};
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server is listening on port http://localhost:${PORT} 🚀`);
