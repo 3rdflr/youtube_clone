cef62b9ac9fd60e073d489f6e50195ec2a0052d2
3rdflr <3rdflrhtl@gmail.com>
2024-11-08 13:50:07 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,18 +4,35 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("<h1></h1>");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 };
 
-const handleLogin = (req, res) => {
-  return res.send("<h1></h1>");
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>You Are Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue");
+  next();
 };
+
+const handleHome = (req, res) => {
+  return res.end();
+};
+
+const handleProtected = (req, res) => {
+  return res.send("You are protected");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
 
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`✅ Server is listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
