25ce716244407894ef1730c67742b317c2e9264f
3rdflr <3rdflrhtl@gmail.com>
2024-11-11 16:44:58 +0900

    updated

--- src/server.js
+++ src/server.js
@@ -1,36 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>You Are Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.end();
-};
-
-const handleProtected = (req, res) => {
-  return res.send("You are protected");
-};
+const logger = morgan("dev");
 
 app.use(logger);
-app.use(privateMiddleware);
 
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`✅ Server is listening on port http://localhost:${PORT} 🚀`);
