eb5dedde985f7ed2179dbc9522fa25d67c742915
3rdflr <3rdflrhtl@gmail.com>
2024-11-13 17:14:02 +0900

    making views

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,10 @@
-export const trending = (req, res) => res.send("Main page");
+export const trending = (req, res) => res.render("home");
 
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
-export const see = (req, res) => {
-  return res.send(`watch video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send("edit");
-};
+export const see = (req, res) => res.render("watch");
+
+export const edit = (req, res) => res.render("Edit");
 export const deleteVid = (req, res) => {
   return res.send("deleteVid");
 };
