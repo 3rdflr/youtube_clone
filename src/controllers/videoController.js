f6dad2908b984c34daedda1f8f3105e640e6598f
3rdflr <3rdflrhtl@gmail.com>
2024-11-15 17:16:47 +0900

    more update..

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,13 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const see = (req, res) => res.render("watch");
 
-export const edit = (req, res) => res.render("Edit");
+export const edit = (req, res) => res.render("edit");
 export const deleteVid = (req, res) => {
   return res.send("deleteVid");
 };
