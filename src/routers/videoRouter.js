cdd5803958bcb050eb2148a779c852fcdede3200
3rdflr <3rdflrhtl@gmail.com>
2024-11-20 19:12:36 +0900

    bad cold

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { see, edit, upload, deleteVid } from "../controllers/videoController";
+import { watch, edit, upload, deleteVid } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVid);
 
