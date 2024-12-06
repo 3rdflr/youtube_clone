31cf1b3d0fc8a263ffe92a872c3fdf7babf1ca7f
3rdflr <3rdflrhtl@gmail.com>
2024-12-06 21:19:58 +0900

    users AUTH

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,17 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/user";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
 
 export const login = (req, res) => res.send("login");
 export const logout = (req, res) => res.send("logout");
