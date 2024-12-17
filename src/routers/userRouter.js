f75437848dee4ba7bda179eb08aaffea33297cf3
3rdflr <3rdflrhtl@gmail.com>
2024-12-17 20:51:27 +0900

    github login is now avaliable.

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  edit,
+  logout,
+  see,
+  githubLogin,
+  githubCallback,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
+
+userRouter.get("/github/login", githubLogin);
+userRouter.get("/github/callback", githubCallback);
 userRouter.get("/:id", see);
 
 export default userRouter;
