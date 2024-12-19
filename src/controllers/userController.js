7851fbe91fcad500617227577172203ad6219f04
3rdflr <3rdflrhtl@gmail.com>
2024-12-19 16:16:04 +0900

    avatar

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -146,9 +146,10 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
+    file,
   } = req;
 
   const currentUser = req.session.user;
@@ -168,6 +169,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
--- src/server.js
+++ src/server.js
@@ -27,6 +27,7 @@ app.use(
   })
 );
 
+app.use("/uploads", express.static("uploads"));
 app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
