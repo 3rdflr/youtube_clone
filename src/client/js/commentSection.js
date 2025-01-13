c0a0efaf47d8bd3decc64c6677ac45a27beecc6d
3rdflr <3rdflrhtl@gmail.com>
2025-01-13 17:35:18 +0900

    hmmmm

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,17 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newCommnets = document.createElement("li");
+  const span = document.createElement("span");
+  newCommnets.className = "video__comments";
+  span.innerText = ` ${text}`;
+  newCommnets.appendChild(span);
+  videoComments.prepend(newCommnets);
+};
+
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
 
@@ -11,7 +21,7 @@ const handleSubmit = (event) => {
   if (text === "") {
     return;
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -19,6 +29,9 @@ const handleSubmit = (event) => {
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
+  if (status === 201) {
+    addComment(text);
+  }
 };
 if (form) {
   form.addEventListener("submit", handleSubmit);
